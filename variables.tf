85ee0ce10465de2bb227da83e698276701ce2d94
Darrs08 <87467049+Darrs08@users.noreply.github.com>
2021-11-03 19:52:08 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,12 @@
 variable "region" {
   type = string
+  default = "us-east-1"
 }
 variable "prefix" {
   type = string
+  default = "client"
 }
 variable "name" {
   type = string
+  default = "samplebucket"
 }
